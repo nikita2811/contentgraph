69b3e7d98d56061a74e38133e19c34557a817b5c
nikita2811 <39294680+nikita2811@users.noreply.github.com>
2026-04-15 11:23:25 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/layouts/DashboardLayout.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
+
+
+
+
+const DashboardLayout: React.FC = () => {
+
+
+    return (
+        <div className="dash-shell">
+            {/* Header */}
+
+            <div className="dash-body">
+                {/* Sidebar */}
+
+                <Sidebar creditsRemaining={50} creditsTotal={170} />
+
+                {/* Page content */}
+                <main className="dash-main">
+                    <Outlet />
+                </main>
+            </div>
+        </div>
+    );
+};
+
+export default DashboardLayout;
